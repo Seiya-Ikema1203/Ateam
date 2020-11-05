ef56adbbdbc5e3610f73cc58b0fdd55210623050
星矢池間1203 <seiya-ikema-19h@stu.kbc.ac.jp>
2020-11-06 07:12:11 +0900

    僕のプロジェクトデータです

---
+++ torutoru_hakihaki/torutoru/Assets/Script/ueshita.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ueshita : MonoBehaviour
+{
+    public float nowPosi;
+    private moveobject mv;
+    
+    void Start()
+    {
+       nowPosi = this.transform.position.y;
+       mv = this.gameObject.GetComponent<moveobject>();
+    }
+
+
+
+    void FixedUpdate()
+    {
+
+        if (mv.InArea== true)
+        {
+            transform.Rotate(new Vector3(10f, -20, 10f));
+            transform.position = new Vector3(transform.position.x, nowPosi + Mathf.PingPong(Time.time / 1, 20.0f), transform.position.z);        }
+    }
+}
