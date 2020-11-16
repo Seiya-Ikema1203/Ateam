cffdb8a6e3a60797644eb2cb5df8bf604724c1e0
星矢池間1203 <seiya-ikema-19h@stu.kbc.ac.jp>
2020-11-16 14:01:35 +0900

    test

---
+++ seiya_project/torutoru_hakihaki/torutoru/Assets/Script/Spiral.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class Spiral : MonoBehaviour
+{
+    public float test;
+
+
+    void Start()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Obj") //moveobjectのrb.massが1になったら子オブジェクトにするようにする
+        {
+            //Debug.Log("aaaaa");
+            //test = other.GetComponent<moveobject>().Getmass();
+            //if (test <= 1f)
+            //{
+                Debug.Log("iiiiiiii");
+                //オブジェクトを子要素に
+                GameObject emptyObject = new GameObject();
+                emptyObject.transform.parent = this.transform;
+                other.transform.parent = emptyObject.transform;
+                emptyObject.name = "object";
+           // }
+        }
+    }
+
+    //void OnTriggerExit(Collider other)
+    //{
+    //    other.transform.parent = null;
+    //    GameObject emptyObject = GameObject.Find("empty");
+    //    Destroy(emptyObject);
+
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ seiya_project/torutoru_hakihaki/torutoru/Assets/Script/moveobject.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveobject : MonoBehaviour
+{
+    //スタートと終わりの目印
+    public Transform startMarker;
+    public Transform endMarker;
+
+    // スピード
+    public float speed = 1.0F;
+
+    //二点間の距離を入れる
+    private float distance_two;
+
+
+    //フラグ用
+    public  bool InArea;
+
+    private Rigidbody rb;
+    //public float thrust = 1.0f;
+
+    void Start()
+    {
+       rb = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+
+        if (endMarker != null)      //終点が入って無ければ動かない
+        {
+
+            // 現在の位置
+            float present_Location = (Time.time * speed) / distance_two;
+             if (present_Location < 0.6f)
+            {
+                // オブジェクトの移動
+                transform.position = Vector3.Lerp(startMarker.position, endMarker.position, present_Location);
+            }
+
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "test")
+        {
+            rb.mass -= 1;
+            if(rb.mass < 1)
+            {
+                rb.useGravity = false; // これをオフにしたら浮くようになる
+
+                InArea = true; //これをオンにしないと上にあげる処理に飛ばない
+                rb.mass = 1;　 //重さを1にしてmassが0にならないようにしている
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "test" && rb.mass <= 1)
+        {
+            //竜巻の範囲内に入ったら終点を自動的に入れる
+            endMarker = other.gameObject.GetComponent<Transform>();
+
+            //二点間の距離を代入(スピード調整に使う)
+            distance_two = Vector3.Distance(startMarker.position, endMarker.position);
+
+            //rb.useGravity = false; // これをオフにしたら浮くようになる
+
+            //InArea = true;
+        }
+
+        if(other.gameObject.tag == "Player" && rb.mass <= 1)
+        {
+            GetComponent<MeshCollider>().enabled = false;
+            this.enabled = false;
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+         if(other.gameObject.tag == "test")
+        {
+            endMarker = null;
+            this.enabled = true;
+            InArea = false;
+        }
+    }
+
+    public float Getmass()
+    {
+        return GetComponent<Rigidbody>().mass; //どこに？あーおｋ
+    }
+}
